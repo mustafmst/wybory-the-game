3c05222b789d62d4008afb0263d834e00e342e09
Paweł <pawel.mstowski@gmail.com>
2019-04-10 21:53:17 +0200

    Added print feature on cards page

--- src/main.js
+++ src/main.js
@@ -3,8 +3,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import ToPaper from "vue-html-to-paper";
 
 Vue.config.productionTip = false;
+
+const options = {
+    name: "_blank",
+    specs: ["fullscreen=yes", "titlebar=yes", "scrollbars=yes"],
+    styles: [
+        "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css",
+        "https://unpkg.com/kidlat-css/css/kidlat.css"
+    ]
+};
+
+Vue.use(ToPaper, options);
 
 new Vue({
     router,
