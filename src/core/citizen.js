a272cd102627ed268ba31714dd7afc3150b6cbd7
Dariusz Rzepka <rzepkadarek@gmail.com>
2019-05-04 17:07:34 +0200

    Citizen - handle cards applying

--- src/core/citizen.js
+++ src/core/citizen.js
@@ -1,16 +1,51 @@
 export default class Citizen {
-    constructor(partiesList) {
-        this.partiesList = partiesList.map(e => ({
-            name: e,
-            score: 0
-        }));
+    politicalViews = new Map();
+    parties = new Map();
+
+    voting = null;
+
+    constructor(politicalViews) {
+        this.__generatePoliticalView(politicalViews)
     }
 
-    applyCard(card) {
+    /**
+     * @param {string[]} politicalViews
+     * @private
+     */
+    __generatePoliticalView(politicalViews) {
+        politicalViews.forEach((view) => {
+            this.politicalViews.set(view, Math.randomInt(-5, 5))
+        })
+    }
+
+    /**
+     * @param {string[]} parties
+     */
+    addParties(parties) {
+        parties.forEach((name) => {
+            this.parties.set(name, {
+                name,
+                score
+            })
+        })
+    }
+
+    /**
+     * @param {string} party
+     * @param {Card} card
+     */
+    applyCard(party, card) {
+        this.parties[party].score += Object.entries(card.params)
+            .reduce((accumulator, [view, value]) => (
+                accumulator + this.politicalViews[view] * value
+            ), 0);
+        this.__updateVoting();
 
     }
 
-    voting() {
-
+    __updateVoting() {
+        const sorted = [...this.parties.values()].sort((partyA, partyB) => partyA.score - partyB.score)
+        console.debug("Voting update", sorted);
+        this.voting = sorted[sorted.length - 1].name;
     }
 }
