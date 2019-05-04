96ccd6c733908b20cb5c81fac4c81e40a8bac38f
Pawel Mstowski <pawel.mstowski@gmail.com>
2019-05-04 15:56:43 +0200

    add citizens

---
+++ src/core/citizen.js
@@ -0,0 +1,17 @@
+export default class Citizen {
+    constructor(partiesList){
+        this.partiesList = partiesList.map(e => (
+            {
+                name: e,
+                score: 0
+            }));
+    }
+
+    applyCard(card){
+
+    }
+
+    voting(){
+        
+    }
+}
