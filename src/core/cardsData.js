2cf5fca0c4965669b40f92e25274a26b818e68d2
Pawel Mstowski <pawel.mstowski@gmail.com>
2019-05-04 20:36:59 +0200

    card data consts

--- src/core/cardsData.js
+++ src/core/cardsData.js
@@ -1,6 +1,14 @@
 import Card from "./card";
+import {
+    KONSERWATYZM,
+    PRAWICA,
+    SOCJALIZM,
+    LEWICA,
+    LIBERALIZM,
+    TUMIWISIZM
+} from "./politicalViews";
 
-const img = "https://ocdn.eu/pulscms-transforms/1/ieqktkqTURBXy9jNTJlZTU3MzExY2QwMGY2ZmVhNjc5ZTQ3M2RiMzdmMC5qcGVnkpUDAADNErHNCoSTBc0Djs0CXw"
+const img = "https://ocdn.eu/pulscms-transforms/1/ieqktkqTURBXy9jNTJlZTU3MzExY2QwMGY2ZmVhNjc5ZTQ3M2RiMzdmMC5qcGVnkpUDAADNErHNCoSTBc0Djs0CXw";
 
 
 export default [
@@ -8,12 +16,12 @@ export default [
         "001",
         "card1",
         {
-            konserwatyzm: 1,
-            prawica: 2,
-            socjalizm: 3,
-            lewica: 4,
-            liberalizm: 5,
-            tumiwisizm: 6
+            [KONSERWATYZM]: 1,
+            [PRAWICA]: 2,
+            [SOCJALIZM]: 3,
+            [LEWICA]: 4,
+            [LIBERALIZM]: 5,
+            [TUMIWISIZM]: 6
         },
         img
     )
--- src/core/politicalViews.js
+++ src/core/politicalViews.js
@@ -1,3 +1,11 @@
-const POLITICAL_VIEWS = ["Konserwatyzm", "Prawica", "Socjalizm", "Lewica", "Liberalizm", "Tumiwisizm"]
+export const KONSERWATYZM = "Konserwatyzm";
+export const PRAWICA = "Prawica";
+export const SOCJALIZM = "Socjalizm";
+export const LEWICA = "Lewica";
+export const LIBERALIZM = "Liberalizm";
+export const TUMIWISIZM = "Tumiwisizm";
+
+
+const POLITICAL_VIEWS = [KONSERWATYZM, PRAWICA, SOCJALIZM, LEWICA, LIBERALIZM, TUMIWISIZM];
 
 export default POLITICAL_VIEWS;
